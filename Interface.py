b9ea7d6bc47e625ce7e38221e98df1b82b58418a
Aloïs BAUDRY <aloisbaudry@gmail.com>
2026-03-24 15:43:15 +0100

    Début de travail sur l'interface

--- Interface.py
+++ Interface.py
@@ -2,10 +2,20 @@ import sys
 from PyQt6 import QtWidgets, uic  # For PyQt6: from PyQt6 import QtWidgets, uic
 
 class MainWindow(QtWidgets.QMainWindow):
+    WVL1_val = None
+    WVL2_val = None
+    Resolution_val = None
+
     def __init__(self):
         super().__init__()
         # Load the .ui file
         uic.loadUi("OSA.ui", self)
+
+        # Default values
+        self.WVL1_val = self.WVL1.value()
+        self.WVL2_val = self.WVL2.value()
+        self.Resolution_val = self.Resolution.value()
+        self.Nb_Points_Set()
 
         # Connect button click to a method
         self.WVL1_Button.clicked.connect(self.WVL1_set)
@@ -14,36 +24,34 @@ class MainWindow(QtWidgets.QMainWindow):
 
 
     def WVL1_set(self):
-        val = self.WVL1.value()  # récupérer la valeur
+        self.WVL1_val = self.WVL1.value()  # récupérer la valeur
         QtWidgets.QMessageBox.information(
             self,
             "Action Triggered",
-            "Start Wavelength set to {}.".format(val)
+            "Start Wavelength set to {:.0f}.".format(self.WVL1_val)
         )
         self.Nb_Points_Set()
     def WVL2_set(self):
-        val = self.WVL2.value()  # récupérer la valeur
+        self.WVL2_val = self.WVL2.value()  # récupérer la valeur
         QtWidgets.QMessageBox.information(
             self,
             "Action Triggered",
-            "End Wavelength set to {}.".format(val)
+            "End Wavelength set to {:.0f}.".format(self.WVL2_val)
         )
         self.Nb_Points_Set()
     def Resolution_set(self):
-        val = self.Resolution.value()  # récupérer la valeur
+        self.Resolution_val = self.Resolution.value()  # récupérer la valeur
         QtWidgets.QMessageBox.information(
             self,
             "Action Triggered",
-            "End Wavelength set to {}.".format(val)
+            "End Wavelength set to {:.2f}.".format(self.Resolution_val)
         )
         self.Nb_Points_Set()
     def Nb_Points_Set(self):
-        WVL1_value = self.WVL1.value()
-        WVL2_value = self.WVL2.value()
-        Resolution_value = self.Resolution.value()
-        if not WVL1_value == WVL2_value or Resolution_value == 0:
-            N_Points = (WVL2_value - WVL1_value)/Resolution_value
-            self.NbPoints.display(N_Points)
+        if self.WVL1_val == self.WVL2_val or self.Resolution_val == 0: return
+
+        N_Points = (self.WVL2_val - self.WVL1_val)/self.Resolution_val
+        self.NbPoints.display(N_Points)
 
 
 if __name__ == "__main__":
