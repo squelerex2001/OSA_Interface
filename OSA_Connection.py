6cde54e715f2050392e64ec4d804a84984845998
squelerex2001 <aloisbaudry@gmail.com>
2026-03-30 14:33:33 +0200

    Visa -> Serial

--- OSA_Connection.py
+++ OSA_Connection.py
@@ -1,18 +1,36 @@
-import pyvisa
-
-OSA_ADDRESS = ""
-
-rm = pyvisa.ResourceManager()
-print(rm.list_resources())
-
-#OSA = rm.open_resource(OSA_ADDRESS)
-#print("OSA IDN = " + OSA.query("*IDN?"))
-
 """
-import pyvisa
-rm = pyvisa.ResourceManager()
-rm.list_resources()
-('ASRL1::INSTR', 'ASRL2::INSTR', 'GPIB0::12::INSTR')
-inst = rm.open_resource('GPIB0::12::INSTR')
-print(inst.query("*IDN?"))
+This program communicate with the AQ6315A OSA
+It uses the GP-IB VISA port with a PROLOGIX GPIB/USB adaptator
+the port is then considered to be a COM Serial port
 """
+import serial
+import time
+
+def write(cmd):
+    OSA.write((cmd + '\n').encode())
+def read():
+    return OSA.read_all().decode()
+def query(cmd):
+    write(cmd)
+    write('++read eoi')
+    time.sleep(0.5)
+    return read()
+def initializePrologix() -> None:
+    write('++mode 1')
+    write('++auto 0')
+    write('++addr 2')
+    write('++eoi 1')
+    write('++eos 3')
+
+# Osa port
+OSA_PORT = "COM5"
+# Opening the OSA port
+OSA = serial.Serial(OSA_PORT, timeout=1)
+
+initializePrologix()
+# Communication test
+print("Start WL:", query('STAWL?'))
+print("Stop WL:", query('STPWL?'))
+
+# Closing the serial port
+OSA.close()
